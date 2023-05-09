c6a1e02ab11373e00cd42bab1924e4ef28204869
Gabriel Iuhasz <iuhasz.gabriel@e-uvt.ro>
2023-05-09 12:12:19 +0300

    updated run scipt

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f ada.yaml -e ada_exp
-/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f rdf.yaml -e rdf_exp
-/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f xgb.yaml -e xgb_exp
-/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f lgbm.yaml -e lgbm_exp
-/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f cat.yaml -e cat_exp
+/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f ada.yaml -e ada_exp_10 -cv 10
+/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f rdf.yaml -e rdf_exp_10 -cv 10
+/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f xgb.yaml -e xgb_exp_10 -cv 10
+/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f lgbm.yaml -e lgbm_exp_10 -cv 10
+/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f cat.yaml -e cat_exp_10 -cv 10
