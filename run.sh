3c5e94110d183dd8403e87a515bf6c4a5dcaa9bc
Gabriel Iuhasz <iuhasz.gabriel@e-uvt.ro>
2023-11-07 13:35:38 +0200

    updated bash script

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@
 /home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f xgb.yaml -e xgb_exp_10 -cv 10
 /home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f lgbm.yaml -e lgbm_exp_10 -cv 10
 /home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f cat.yaml -e cat_exp_10 -cv 10
+/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f dnn.yaml -e dnn_exp_10 -cv 10
+/home/ubuntu/miniconda3/envs/pyml/bin/python sr_energy_experiments.py -f cnn.yaml -e cnn_exp_10 -cv 10
